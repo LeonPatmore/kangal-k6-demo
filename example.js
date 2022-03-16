3b89a72e3144f680fc6446965478b040589ce81a
Leon Patmore <leon.patmore@bath.edu>
2022-03-16 22:24:37 +0000

    trying to setup kangal reports.

--- example.js
+++ example.js
@@ -3,3 +3,18 @@ import http from "k6/http";
 export default function () {
   let response = http.get("http://simple-http.default:80");
 }
+
+export function handleSummary(data) {
+  console.log("Preparing the end-of-test summary...");
+
+  if (!__ENV.REPORT_PRESIGNED_URL) {
+    return;
+  }
+  const resp = http.put(__ENV.REPORT_PRESIGNED_URL, JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (resp.status != 200) {
+    console.error("Could not send summary, got status " + resp.status);
+  }
+}
